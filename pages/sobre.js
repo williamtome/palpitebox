fa01b9cbbf0a46865b03d9ad00fe7f2f4fae95cf
William Tomé <williamtome@DESKTOP-5JFV53L.localdomain>
2020-08-28 23:12:39 -0300

    create sobre page

--- pages/sobre.js
+++ pages/sobre.js
@@ -4,10 +4,15 @@ import PageTitle from '../components/PageTitle'
 
 const Sobre = () => {
   return (
-    <div>
+    <div className='pt-16 pb-16'>
       <PageTitle title='Sobre' />
-      <h1>Sobre</h1>
-      <div>
+      <div className='container mx-auto'>
+        <p className='text-center'>
+          Palpitebox é um projeto desenvolvido dentro do treinamento <b>"Fullstack Master"</b> do DevPleno, ministrado pelo mestre Túlio Faria,
+          <br/> que também foi usado como projeto base durante a Semana Fullstack Master realizada em julho de 2020.
+        </p>
+      </div>
+      <div className='text-center pt-10 text-green-800'>
         <Link href="/">
           <a>Home</a>
         </Link>
