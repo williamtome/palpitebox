5d3ee7c310beeb5eed2c9cf4b3c7fbacaeb31422
William Tomé <williamtome@DESKTOP-5JFV53L.localdomain>
2020-09-02 14:10:08 -0300

    fix contato page.

--- pages/contato.js
+++ pages/contato.js
@@ -4,10 +4,19 @@ import PageTitle from '../components/PageTitle'
 
 const Contato = () => {
   return (
-    <div>
-      <PageTitle title='COntato' />
-      <h1>Contato</h1>
-      <div>
+    <div className='pt-16 pb-16'>
+      <PageTitle title='Contato' />
+      <div className='container mx-auto text-center'>
+        <div className='mb-6'>
+          <strong>E-mail:</strong>
+          <p>william.tomenh@gmail.com</p>
+        </div>
+        <div className='mt-6'>
+          <strong>Contato/Whatsapp:</strong>
+          <p>(51) 99303.8350</p>
+        </div>
+      </div>
+      <div className='text-center pt-10 text-green-800'>
         <Link href="/">
           <a>Home</a>
         </Link>
