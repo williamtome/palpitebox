cf0968c4a1319847f78ff4d1d2e1ed5a9a716bdd
William Tomé <william.tomenh@gmail.com>
2021-03-07 21:43:57 -0300

    Implementando validação do formulário com formik e yup.

--- pages/pesquisa.js
+++ pages/pesquisa.js
@@ -1,58 +1,60 @@
 import React, { useState } from 'react'
 import PageTitle from '../components/PageTitle'
 import * as Yup from 'yup'
+import { useFormik } from 'formik'
+
+const PesquisaSchema = Yup.object().shape({
+  Nome: Yup
+    .string()
+    .required('Nome é obrigatório!'),
+  Email: Yup
+    .string()
+    .email()
+    .required('Email é obrigatório!'),
+  Whatsapp: Yup
+    .string()
+    .required('Whatsapp é obrigatório!'), 
+  Critica: Yup
+    .string()
+    .required('Crítica é obrigatório!'), 
+  Nota: Yup
+    .number()
+    .positive()
+    .required('Nota é obrigatório!'),
+  Indica: Yup
+    .string()
+    .required('Indica é obrigatório!')
+})
+
 
 const Pesquisa = () => {
-  
-  const [ form, setForm ] = useState({
-    Nome: '',
-    Email: '',
-    Whatsapp: '', 
-    Critica: '', 
-    Nota: 0,
-    Indica: ''
-  })
-
   const notas = [0, 1, 2, 3, 4, 5]
   const indicacoes = ['Sim', 'Não']
   const [success, setSuccess] = useState(false)
   const [retorno, setRetorno] = useState({})
-  // console.log(form);
-
-  const PesquisaSchema = Yup.object().shape({
-    Nome: Yup
-      .string()
-      .required('Nome é obrigatório!'),
-    Email: Yup
-      .string()
-      .email()
-      .required('Email é obrigatório!'),
-    Whatsapp: Yup
-      .string()
-      .required('Whatsapp é obrigatório!'), 
-    Critica: Yup
-      .string()
-      .required('Crítica é obrigatório!'), 
-    Nota: Yup
-      .number()
-      .positive()
-      .required('Nota é obrigatório!'),
-    Indica: Yup
-      .string()
-      .required('Indica é obrigatório!')
+  
+  const formik = useFormik({
+    initialValues: {
+      Nome: '',
+      Email: '',
+      Whatsapp: '', 
+      Critica: '', 
+      Nota: 0,
+      Indica: ''
+    },
+    validationSchema: { PesquisaSchema },
+    onSubmit: values => {
+      console.log(values)
+      save
+      setSubmitting(false)
+    }
   })
-
-  const formData = PesquisaSchema.cast(form)
-  console.log(formData);
-  PesquisaSchema.isValid(formData)
-    .then(valid => console.log('É válido ? ', valid))
-    .catch(err => console.log(err))
   
   const save = async() => {
     try {
       const response = await fetch('/api/save', {
         method: 'post',
-        body: JSON.stringify(form)
+        body: JSON.stringify(formik.values)
       })
       const data = await response.json()
       setSuccess(true)
@@ -61,14 +63,14 @@ const Pesquisa = () => {
     }
   }
 
-  const onChange = event => {
-    const value = event.target.value
-    const key = event.target.name
-    setForm(old => ({
-      ...old,
-      [key]: value
-    }));
-  }
+  // const onChange = event => {
+  //   const value = event.target.value
+  //   const key = event.target.name
+  //   setForm(old => ({
+  //     ...old,
+  //     [key]: value
+  //   }));
+  // }
   return (
     <div className="pt-6">
       <PageTitle title='Pesquisa' />
@@ -80,65 +82,87 @@ const Pesquisa = () => {
         Por isso, estamos sempre abertos a ouvir sua opinião.
       </p>
       {!success && <div className='w-1/4 mx-auto'>
-        <label className='font-bold'>Seu nome:</label>
-        <input type="text" 
-          className='w-full p-4 block shadow bg-blue-100 my-2 rounded' 
-          placeholder='Nome' 
-          onChange={onChange} 
-          name='Nome' 
-          value={form.Nome} />
+        <form onSubmit={formik.handleSubmit}>
+          <label className='font-bold'>Seu nome:</label>
+          <input 
+            type="text" 
+            className='w-full p-4 block shadow bg-blue-100 my-2 rounded' 
+            placeholder='Nome' 
+            onChange={formik.handleChange} 
+            name='Nome' 
+            value={formik.values.Nome}
+          />
+          {formik.errors.Nome ? (
+            <div>{formik.errors.Nome}</div>
+          ) : null}
 
-        <label className='font-bold'>E-mail:</label>
-        <input type="text" 
-          className='w-full p-4 block shadow bg-blue-100 my-2 rounded' 
-          placeholder='E-mail' 
-          onChange={onChange} 
-          name='Email' 
-          value={form.Email}/>
+          <label className='font-bold'>E-mail:</label>
+          <input 
+            type="text" 
+            className='w-full p-4 block shadow bg-blue-100 my-2 rounded' 
+            placeholder='E-mail' 
+            onChange={formik.handleChange} 
+            name='Email' 
+            value={formik.values.Email}
+          />
+          {formik.errors.Email ? (
+            <div>{formik.errors.Email}</div>
+          ) : null}
 
-        <label className='font-bold'>Whatsapp:</label>
-        <input type="text" 
-          className='w-full p-4 block shadow bg-blue-100 my-2 rounded' 
-          placeholder='Whatsapp' 
-          onChange={onChange} 
-          name='Whatsapp' 
-          value={form.Whatsapp}/>
+          <label className='font-bold'>Whatsapp:</label>
+          <input 
+            type="text" 
+            className='w-full p-4 block shadow bg-blue-100 my-2 rounded' 
+            placeholder='Whatsapp' 
+            onChange={formik.handleChange} 
+            name='Whatsapp' 
+            value={formik.values.Whatsapp}
+          />
+          {formik.errors.Whatsapp ? (
+            <div>{formik.errors.Whatsapp}</div>
+          ) : null}
 
-        <label className='font-bold'>Sua crítica ou sugestão:</label>
-        <input type="text" 
-          className='w-full p-4 block shadow bg-blue-100 my-2 rounded' 
-          placeholder='Descreva aqui.'
-          onChange={onChange} 
-          name='Critica' 
-          value={form.Critica}/>
+          <label className='font-bold'>Sua crítica ou sugestão:</label>
+          <input 
+            type="text" 
+            className='w-full p-4 block shadow bg-blue-100 my-2 rounded' 
+            placeholder='Descreva aqui.'
+            onChange={formik.handleChange} 
+            name='Critica' 
+            value={formik.values.Critica}
+          />
+          {formik.errors.Critica ? (
+            <div>{formik.errors.Critica}</div>
+          ) : null}
 
-        <label className='font-bold'>Nota:</label>
-        <div className='flex py-6'>
-          {
-            notas.map(nota => {
-              return (
-                <div key={nota} className='block w-1/6 text-center'> 
-                  {nota} <br/>
-                  <input type="radio" name='Nota' value={nota} onChange={onChange}/> 
-                </div>
-              )
-            })
-          }
-        </div>
-        <label className='font-bold'>Você nos indicaria para um(a) amigo(a)?</label>
-        <div className='flex py-6'>
-          {
-            indicacoes.map(indicacao => {
-              return (
-                <div key={indicacao} className="block w-1/2 text-center">
-                  {indicacao} <br/>
-                  <input type="radio" name="Indica" value={indicacao} onChange={onChange}/>
-                </div>
-              )
-            })
-          }
-        </div>
-        <button className='w-full bg-blue-400 px-12 py-4 font-bold rounded-lg shadow-lg hover:shadow mb-6' onClick={save}>Enviar</button>        
+          <label className='font-bold'>Nota:</label>
+          <div className='flex py-6'>
+            {
+              notas.map(nota => {
+                return (
+                  <div key={nota} className='block w-1/6 text-center'> 
+                    {nota} <br/>
+                    <input type="radio" name='Nota' value={nota} onChange={formik.handleChange}/> 
+                  </div>
+                )
+              })
+            }
+          </div>
+          <label className='font-bold'>Você nos indicaria para um(a) amigo(a)?</label>
+          <div className='flex py-6'>
+            {
+              indicacoes.map(indicacao => {
+                return (
+                  <div key={indicacao} className="block w-1/2 text-center">
+                    {indicacao} <br/>
+                    <input type="radio" name="Indica" value={indicacao} onChange={formik.handleChange}/>
+                  </div>
+                )
+              })
+            }
+          </div>
+          <button className='w-full bg-blue-400 px-12 py-4 font-bold rounded-lg shadow-lg hover:shadow mb-6'>Enviar</button>        
+        </form>
       </div>}
       {success && <div className='w-1/5 mx-auto'>
         <p className='mb-6 text-center bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3'>Obrigado por contribuir com sua sugestão ou crítica.</p>
